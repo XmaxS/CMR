3771e9a2ea78263e1d5d12b878d9b62e8549cebc
B2537 <2537993844@qq.com>
2019-11-23 18:13:13 +0800

    2019.11.23 极简ui完成

--- app/src/main/java/com/sakura/cmr/CmrActivity.java
+++ app/src/main/java/com/sakura/cmr/CmrActivity.java
@@ -1,7 +1,16 @@
 package com.sakura.cmr;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CmrActivity extends AppCompatActivity {
 
@@ -9,5 +18,57 @@ public class CmrActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+
+
+    }
+
+
+    /**
+     * 判断需要的权限是否全部征得用户同意，否则退出
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0){
+                    for (int result : grantResults){
+                        if (result != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"权限不足，app无法正常运行!",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                }else {
+                    Toast.makeText(this,"出现未知错误!",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    /**
+     * 判断运行时权限是否申请到，没申请到一起申请
+     */
+    private void checkPermission(){
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(CmrActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (ContextCompat.checkSelfPermission(CmrActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            //一次性申请集合内的权限
+            ActivityCompat.requestPermissions(CmrActivity.this,permissions,1);
+        }
     }
 }
